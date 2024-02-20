0274824ca3aca9363e7bc1211bd1ca9a2b355a25
dotnetbabu <ayush06092002@gmail.com>
2024-02-21 00:30:26 +0530

    Implemented a back button

--- app/src/main/java/com/example/filmflow/screens/details/MovieDetailScreen.kt
+++ app/src/main/java/com/example/filmflow/screens/details/MovieDetailScreen.kt
@@ -1,31 +1,69 @@
 package com.example.filmflow.screens.details
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
 fun MovieDetailScreen(navController: NavController, movieData: String?) {
     //enter the composable function for the MovieDetailScreen
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight(),
-        color = Color(0xFF87b6a7)) {
-        Column(modifier = Modifier,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
-            Text(text = movieData.toString(), style = MaterialTheme.typography.headlineMedium)
-
+    Scaffold(
+        topBar = {
+            TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(0xFFe3f09b)
+            ), title = {
+                    Text(text = "Movies",
+                        style = TextStyle(color = Color.Black)
+                    )
+                },
+                navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(Icons.Filled.ArrowBack, contentDescription = "Go back", tint = Color.Black)
+                }
+            })
+        }
+    ) {
+        Surface(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            color = Color(0xFF87b6a7)) {
+            Column(modifier = Modifier,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                Text(text = movieData.toString(), style = MaterialTheme.typography.headlineMedium)
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(onClick = { navController.popBackStack() }) {
+                    Text(text = "Go Back")
+                }
+            }
         }
     }
+
+
 }
